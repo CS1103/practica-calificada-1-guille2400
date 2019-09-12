7c0aee1ed69f834343324dbf30e5279d3557d5ed
Guillermo Sanchez <49462669+guille2400@users.noreply.github.com>
2019-09-12 18:06:32 -0500

    Add files via upload

--- Polynomial.cpp
+++ Polynomial.cpp
@@ -5,3 +5,52 @@
 #include <stdexcept>
 #include <iostream>
 #include "Polynomial.h"
+using  namespace std;
+
+int Polynomial::degree() {
+    auto it = arreglo->end()-1;
+    cout<< *it;
+}
+
+auto Polynomial::multiplicarPolinomioConsigoMismo(vector<double>* array1)
+{
+    auto first = array1->begin();
+    auto last = array1->end();
+    float temp = 0;
+
+    for(auto i = first; i != last ; i++)
+    {
+
+    }
+}
+
+auto Polynomial::sumarPolinomios(vector<double>* array1,vector<double>* array2) {
+    auto first = array1->begin();
+    auto last = array1->end();
+    auto first1 = array2->begin();
+    auto last1 = array2->end();
+    float temp = 0;
+    int a=1;
+    int sum=0;
+    while (a%2!=0)
+    {
+        for (auto i = first; i != last; i++)
+        {
+            for (auto k = first1; i!=last; i++)
+            {
+                sum=*i+*k;
+            }
+        }
+        a++;
+    }
+}
+auto Polynomial::multiplicarPorConstante(vector<double>* array1,double constante)
+{
+    auto it = array1->begin();
+    cout<< (*it)*constante;
+}
+auto Polynomial::sumaPolinomioConConstante(vector<double>* array1,double constante)
+{
+    auto it = array1->begin();
+    cout<< (*it)+constante;
+}
--- Polynomial.h
+++ Polynomial.h
@@ -6,9 +6,31 @@
 #define POLINOMIO_POLINOMIO_H
 
 #include <vector>
+#include <ostream>
+#include <iostream>
 using namespace std;
 
 class Polynomial {
+private:
+    vector<double>* arreglo;
+public:
+    Polynomial() {
+        arreglo = new vector<double>();
+    }
+    void add(double elem, int exp )
+    {
+        arreglo->push_back(elem);
+        arreglo->push_back(exp);
+    }
+
+    int degree();
+    auto multiplicarPolinomioConsigoMismo(vector<double>*);
+    auto sumarPolinomios(vector<double>*,vector<double>*);
+    auto multiplicarPorConstante(vector<double>*, double);
+    auto sumaPolinomioConConstante(vector<double>* array1,double constante);
+    auto operator + (const int& xInteger)
+    {
+    }
 
 };
 
